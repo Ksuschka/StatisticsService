a235bea222dfb97578af397400f8b1d7c8b053e6
Anisimova Oxana <ksuschka.an@gmail.com>
2021-10-06 11:22:14 +0300

    fixes

--- src/test/java/ru/netology/StatisticsService/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsService/StatisticsServiceTest.java
@@ -10,7 +10,19 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12,5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax100() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {8,5, 12, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
